3e308aecb0394e1dd3ee71406a4de2be8cdab0d7
Linda J <66274983+Lindajeanph@users.noreply.github.com>
2020-06-22 19:12:16 -0400

    Java drill

---
+++ basic_javascript_1.js
@@ -0,0 +1,28 @@
+
+
+document.write("hello, world");
+
+var A= "I don't know"
+
+document.write(A);
+
+var bunny= "the cutest"
+
+window.alert(bunny);
+
+document.write("\"this is a story of a girl, who cried a river and drowned the whole world\"");
+
+document.write("\"to be, or not to be." + "that is the question" + "isn\'t it?\"");
+
+var b = "concatenated" + "string" //assign variable with letter b
+document.write(b)       //added 2 words to make 1 word
+            //putting the value of the result together
+
+var C="15", L="14", V="200"
+document.write(L);
+
+var C= "Caleb likes the color blue";
+var C= C.fontcolor("blue");
+document.write(C);
+
+document.write(25+25);
