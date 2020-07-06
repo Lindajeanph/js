2fcd697720725d6051fe849693ee44d88a061f46
Linda J <66274983+Lindajeanph@users.noreply.github.com>
2020-07-06 10:39:32 -0400

    Completed pg 129

--- basic_javascript_6.js
+++ basic_javascript_6.js
@@ -1,13 +1,14 @@
+
 function assignment() {
     var age, you_can;
-    age= document.getElementById("age").Value;
+    age= document.getElementById("age").value;
     you_can = (age < 18) ? "too young":"you are old enough";
     document.getElementById("vote").innerHTML = you_can + " to vote.";
 }
 
 function vehicle(make, model, year, color) {
-    this.vehicle_make = make;
-    this.vehicicle_model = model;
+    this.vehicle_make = make;           //KEYWORDS AND CONSTRUCTORS//
+    this.vehicle_model = model;
     this.vehicle_year = year;
     this.vehicle_color = color;
 }
@@ -20,11 +21,23 @@ function myfunction() {
     "erik drives a " + erik.vehicle_color + "-colored " + erik.vehicle_model + "manufactured in" + erik.vehicle_year;
 }
 
-function nesting() {
-    document.getElementById("nested_function").innerHTML = count();
+function phone(type, model, color) {
+    this.phone_type = type;
+    this.phone_model = model;
+    this.phone_color = color;
+}
+var verizon = new phone("iphone", "XR", "rosegold");
+var sprint = new phone("android", "galaxy", "white");               //my phone example//
+function cellphone() {
+    document.getElementById("new_and_this").innerHTML =
+    "sprint also has the " + verizon.phone_type + " in green";
+}
+
+function countingfunction() {
+    document.getElementById("counting").innerHTML = count();
     function count() {
         var starting_point = 9;
-        function plus_1() {starting += 1;}
+        function plus_1() {starting_point += 1;}
         plus_1();
         return starting_point;
     }
